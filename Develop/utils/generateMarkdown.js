498cfa54ba45d3c2ab2946fb7493e2fbae5b5ea8
Anthony Codinach <ant@imac.lan>
2023-06-05 22:17:48 -0400

    fixed debug issue wiith license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,27 +1,27 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license === "None") {
+function renderLicenseBadge(licences) {
+  if (licences === "None") {
     return "";
   } else {
-    return `![Github license](https://img.shields.io/badge/license-${license}-brightgreen.svg)`;
+    return `![Github license](https://img.shields.io/badge/license-${licences}-brightgreen.svg)`;
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { 
-  if (license === "None") {
+function renderLicenseLink(licences) { 
+  if (licences === "None") {
     return "";
   } else {
-    return `(https://choosealicense.com/licenses/${license}/)`;
+    return `(https://choosealicense.com/licenses/${licences}/)`;
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === "None") {
+function renderLicenseSection(licences) {
+  if (licences === "None") {
     return "";
   } else {
     return `## License`
@@ -54,11 +54,11 @@ ${data.usageInformation}
 
 ${data.contributionGuidelines}
 
-${renderLicenseSection(data.license)}
-${renderLicenseLink(data.license)}
+${renderLicenseSection(data.licences)}
+${renderLicenseLink(data.licences)}
 
 ## Badges
-${renderLicenseBadge(data.license)}
+${renderLicenseBadge(data.licences)}
 
 ## Tests
 
